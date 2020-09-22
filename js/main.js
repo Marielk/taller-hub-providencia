fb713761cb1e2a0344d7c65096946dfffeb851ab
Mariel Quezada <mariel@yapp.cl>
2020-09-22 11:08:26 -0300

    proyecto basico

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+import { VanillaCalendar }  from './vanilla-calendar-min.js'
+window.VanillaCalendar=VanillaCalendar;
+
+//tomar valores del DOM
+// formulario
+// cuando el documento este listo detectar cuando el formulario es enviado
+// validar que los campos son correctos
+// guardar informacion en alguna parte (por ahora en local storage)
+// mostrar informacion de confirmación al usuario
+
+
+ //calendario
+
+ 
+
+
